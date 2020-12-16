f3afbb060293433c03e35d057736880af3082344
Waldemar Jackiewicz <focus69a@gmail.com>
2020-12-16 15:01:46 +0100

    zrobiona tabela z bindowaniem atrybutow
    nie uzywam wierszy - sa do usuniecia
    zrobione wyswietlanie danych w tabeli dynamicznie, ale bez akcji
    zrobiony podglad obrazka w panelu prawym

--- src/app/dashboard/tabela/tabela.component.ts
+++ src/app/dashboard/tabela/tabela.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabelaComponent implements OnInit {
 
-  constructor() { }
+  znacznikWyswietlania: number[] = [0,1,   0,0,   0,0,   0, 0,  0,0];
+  znacznik: string = "===>>";
+
+  alertTyp: string[] = ["Brak osłony twarzy", "Podwyższona temperatura"];
+  tabela10Komunikatow: number[] = [0,0, 1,1, 0,0, 1,1, 0,0];
+
+  czasJakiUplynal: number[] = [1, 10, 12, 16, 90, 360, 420, 520, 800, 950];
+  numerKamery: number[] = [1, 5, 3, 3, 5, 1, 2, 2, 10, 5];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
-  }
+}
 
 }
