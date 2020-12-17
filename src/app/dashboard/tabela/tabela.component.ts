879f6cd090c56a869f9320332a28d529057e677e
Waldemar Jackiewicz <focus69a@gmail.com>
2020-12-17 14:57:12 +0100

    dzialaja klawisze kolory w zaleznosci od wartosci w tabeli 20201217 czw

--- src/app/dashboard/tabela/tabela.component.ts
+++ src/app/dashboard/tabela/tabela.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Tabela} from './tabela.model';    //dodalem model danych z klasy Tabela.model
 
 @Component({
   selector: 'app-tabela',
@@ -7,14 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabelaComponent implements OnInit {
 
-  znacznikWyswietlania: number[] = [0,1,   0,0,   0,0,   0, 0,  0,0];
-  znacznik: string = "===>>";
+  numerKamery: number[] = [1, 5, 3, 3, 5, 1, 2, 2, 10, 5];
+  czasJakiUplynal: number[] = [1, 10, 12, 16, 90, 360, 420, 520, 800, 950];
 
   alertTyp: string[] = ["Brak osłony twarzy", "Podwyższona temperatura"];
   tabela10Komunikatow: number[] = [0,0, 1,1, 0,0, 1,1, 0,0];
 
-  czasJakiUplynal: number[] = [1, 10, 12, 16, 90, 360, 420, 520, 800, 950];
-  numerKamery: number[] = [1, 5, 3, 3, 5, 1, 2, 2, 10, 5];
+  znacznikStyluKlawisza: number[] = [0,0,   1,1,   1,1,   1, 1,  0,1];  //0 - wyswietl  1 - juz ocenione
+
+  znacznikWyswietlania: number[] = [0,1,   0,0,   0,0,   0, 0,  0,0];
+  znacznik: string = "===>>";
+
+
+
+  TabelaZdarzen: Tabela[] = [                  // a tutaj go uzylem, powstal nowy obiekt Tabela zapisany w tabeli zdarzen
+    new Tabela('1', '10', this.alertTyp[0], '?')
+  ];
 
   constructor() {
   }
